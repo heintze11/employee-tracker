012e32e2e37620ac55faf6c6fed62a0de3637dd6
Levi H <levi.heintzelman@gmail.com>
2022-09-09 14:57:32 -0600

    Add sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO departments (name)
+VALUES ("Accounting"),
+       ("Software"),
+       ("Operations"),
+       ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 80000, 1),
+       ("Associate Engineer", 120000, 2),
+       ("Sales Lead", 100000, 4),
+       ("Salesman", 80000, 4),
+       ("Senior Engineer", 150000, 2),
+       ("Technician", 80000, 3),
+       ("Specialist", 90000, 3),
+       ("CPA", 110000, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Boris", "Miller", 4, 3),
+       ("Lisa", "MacDonald", 4, 3),
+       ("Owen", "Cornish", 3),
+       ("Gavin", "Piper", 3, 3),
+       ("Brian", "Bower", 2, 6),
+       ("Kimberly", "Davidson", 5),
+       ("Adrian", "Mills", 2, 6),
+       ("Anna", "Springer", 5, 6),
+       ("Sebastian", "Robertson", 6, 11),
+       ("Andrea", "Sanderson", 6, 11),
+       ("Jonathan", "Turner", 7),
+       ("Anne", "Mills", 7, 11),
+       ("Jake", "Sanderson", 1, 14),
+       ("Amanda", "Ellison", 8),
+       ("Alexander", "Forsyth", 1, 14),
+       ("Benjamin", "North", 8, 14);
---
+++ server.js
@@ -0,0 +1,11 @@
+// Install dependencies
+    // Express
+    // Inquirer 8.2.4
+    // mysql2
+    // Console.table
+// Create Database
+// Seed Database
+// Use inquirer to select from query options
+// Use console.table to show results...?
+
+
